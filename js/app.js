cec3f2ed019359a0d76fb0699a1ec378b2de4332
georgealv96 <george1996alvarado@gmail.com>
2023-06-14 14:15:09 -0600

    added render functions and styling

---
+++ js/app.js
@@ -0,0 +1,86 @@
+/*----- constants -----*/
+const COLORS = {
+  0: 'white',
+  1: 'purple',
+  '-1': 'orange'
+}
+
+/*----- state variables -----*/
+
+let board // array of 7 column arrays
+let turn // 1 or -1
+let winner // null = no winner; 1 or -1 winner; 'T' = Tied game
+
+/*----- cached elements  -----*/
+const messageEl = document.querySelector('h1')
+const playAgainBtnEl = document.querySelector('button')
+const markerEls = document.querySelectorAll('#markers > div')
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
+init()
+
+// Initialize all state, then call render()
+function init() {
+  // To visualize the board's mapping to the DOM,
+  // rotate the board array 90 degrees counter-clockwise
+  board = [
+    [0, 0, 0, 0, 0, 0], // col 0
+    [0, 0, 0, 0, 0, 0], // col 1
+    [0, 0, 0, 0, 0, 0], // col 2
+    [0, 0, 0, 0, 0, 0], // col 3
+    [0, 0, 0, 0, 0, 0], // col 4
+    [0, 0, 0, 0, 0, 0], // col 5
+    [0, 0, 0, 0, 0, 0] // col 6
+  ]
+  turn = 1
+  winner = null
+  render()
+}
+
+// Visualize all state in the DOM
+function render() {
+  renderBoard()
+  renderMessage()
+  // Hide/show UI elements (controls)
+  renderControls()
+}
+
+function renderBoard() {
+  board.forEach(function (colArr, colIdx) {
+    // Iterate over the cells in the current column (colArray)
+    colArr.forEach(function (cellVal, rowIdx) {
+      const cellId = `c${colIdx}r${rowIdx}`
+      const cellEl = document.getElementById(cellId)
+      cellEl.style.backgroundColor = COLORS[cellVal]
+    })
+  })
+}
+
+function renderMessage() {
+  if (winner === 'T') {
+    messageEl.innerText = "It's a Tie!!!"
+  } else if (winner) {
+    messageEl.innerHTML = `<span style="color: ${COLORS[winner]}">${COLORS[
+      winner
+    ].toUpperCase()}</span>' Wins!`
+  } else {
+    // Game is in play
+    messageEl.innerHTML = `<span style="color: ${COLORS[turn]}">${COLORS[
+      turn
+    ].toUpperCase()}</span>'s Turn`
+  }
+}
+
+function renderControls() {
+  // Ternary expression is the go to when you want 1 of 2 values returned
+  // <conditional expression> ? <truthy> : <falsy>
+  playAgainBtnEl.style.visibility = winner ? 'visible' : 'hidden'
+  // Iterate over the marker elements to hide/show
+  // according to the column being full (no 0's) or not
+  markerEls.forEach(function (markerEl, colIdx) {
+    const hideMarker = !board[colIdx].includes(0) || winner
+    markerEl.style.visibility = hideMarker ? 'hidden' : 'visible'
+  })
+}
